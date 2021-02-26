2377cdfe76780f469c36f34dff4e40663ea59c39
Sam North <sam.north@fiscalnote.com>
2021-02-26 10:35:53 -0600

    -removed an unused import

--- UI.Web/ClientApp/src/app/components/examples/crud-examples/crud-examples.component.ts
+++ UI.Web/ClientApp/src/app/components/examples/crud-examples/crud-examples.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CRUDExample } from 'src/app/models/CRUDExample';
